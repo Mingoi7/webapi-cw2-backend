829dd655114f3fa7e74de0d476a6afad4e34d1d4
mingoi7 <minglovengo@gmail.com>
2024-06-18 23:05:06 +0800

    final commit

---
+++ config.ts
@@ -0,0 +1,26 @@
+export const config = {
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: '1234',
+  database: "postgres",
+}
+
+/*
+export const config = {
+  host: "satao.db.elephantsql.com",
+  port: 5432,
+  user: "iggsofsv",
+  password: "0yR10FxhXy7ykcQPaQ9Ec5guGo1o7ETF",
+  database: "iggsofsv",
+  connection_limit: 100
+}
+
+export const configMongo = {
+  user: 'testuser',
+  pwd:  'mongoDB1688',
+  host: '304cem.jau1i.mongodb.net',
+  dbname: 'blog'
+}
+
+*/
